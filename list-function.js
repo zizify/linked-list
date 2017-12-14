889b3a024a79cb83a17fdfef273f076c04c08493
Adam Bradbury <f.adambrad@gmail.com>
2017-12-14 12:06:45 -0700

    completed exercise 2

--- list-function.js
+++ list-function.js
@@ -3,11 +3,13 @@ const LinkedList = require('./linked-list')
 
 
 const linkedList = new LinkedList
+const linkList2 = new LinkedList
 
 linkedList.insert(0, 'A')
 linkedList.insert(1, 'B')
 linkedList.insert(2, 'C')
 linkedList.insert(3, 'D')
+
 
 
 // displays the linked list
@@ -20,7 +22,7 @@ function printList(linkedList) {
 }
 
 
-printList(linkedList)
+// printList(linkedList)
 
 function listLength(linkedList){
     let i = 0;
@@ -32,4 +34,36 @@ function listLength(linkedList){
     return i
 }
 
-console.log(listLength(linkedList, 'listLength'))
+// console.log(listLength(linkedList, 'listLength'))
+
+function isEmpty(linkedList) {
+    if (linkedList.head === null){
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+
+// console.log(isEmpty(linkList2))
+
+
+// findPrevious: finds the node before the item you are looking for
+function findPrevious(linkedList, index){
+    return linkedList.get(index - 1)
+}
+
+// console.log(findPrevious(linkedList, 3))
+
+function findLast(linkedList){
+    let i = 0;
+    let currentNode = linkedList.head;
+    while (currentNode !== null) {
+        i++
+        currentNode = currentNode.next
+    }
+    return linkedList.get(i - 1)
+}
+
+console.log(findLast(linkedList))
